7714a23d52389cf2e475bf950b99a52580f15b4e
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-04-22 04:30:49 +1200

    Dual hand grabbing, Focus on dual hand
    Made the hand movement with the sword held with 2 hands a lot nicer.
    Also added more grab points to the boombox.

--- Scripts/controllerDefaults.gd
+++ Scripts/controllerDefaults.gd
@@ -4,6 +4,7 @@ signal gripped
 signal released
 
 @export var threshold: float = 0.9
+@export var handParticles: GPUParticles3D
 
 @export var is_gripped: bool:
 	set(value):
@@ -14,10 +15,10 @@ signal released
 		is_gripped = value
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta):
+func _physics_process(delta):
 	is_gripped = get_float("grip") > threshold and get_float("trigger") > threshold
 	if(is_gripped):
-		$GPUParticles3D.emitting = true
+		handParticles.emitting = true
 	else:
-		$GPUParticles3D.emitting = false
+		handParticles.emitting = false
 	#print(name,value)
