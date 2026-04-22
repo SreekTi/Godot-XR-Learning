310a3f91a5daf4b747c43412de84c430ebf4342a
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-04-22 13:20:27 +1200

    Sword interaction progress
    Attempting to get input for the sword, to add fire on blade soon

---
+++ Scenes/sword.gd
@@ -0,0 +1,6 @@
+extends XRToolsPickable
+
+#func _physics_process(delta: float) -> void:
+	#is_gripped = get_float("grip") > threshold and get_float("trigger") > threshold
+	#if is_picked_up() and controller_action(): #XRController3D.is_button_pressed("ax_button"):
+		#pass
--- Scripts/musicMenu.gd
+++ Scripts/musicMenu.gd
@@ -1,16 +1,11 @@
 extends Control
 
-
-
-
 func _on_jackpot_pressed():
 	Globals.speakerSong.emit("res://songs/JACKPOT.mp3")
 
 
 func _on_aizo_pressed():
 	Globals.speakerSong.emit("res://songs/King Gnu - AIZO.mp3")
-
-
 
 func _on_volume_value_changed(value):
 	Globals.speakerVolume = value
